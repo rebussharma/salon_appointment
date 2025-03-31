15f4c131de2d42717f09596c7fa6245f5c83705a
Ribash Sharma <ribashsharma@Ribashs-MBP.attlocal.net>
2025-03-31 13:53:09 -0500

    index re-do

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
 
-const root = ReactDOM.createRoot(
+ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
-root.render(
+).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
